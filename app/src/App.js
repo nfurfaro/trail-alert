75233e31828d1576f98684aac54ea87070daacc8
Nick Furfaro <nfurfaro33@gmail.com>
2017-04-06 21:28:13 -0600

    linted code.

--- app/src/App.js
+++ app/src/App.js
@@ -1,115 +1,108 @@
 import RaisedButton from 'material-ui/RaisedButton';
-import {grey900} from 'material-ui/styles/colors';
+import { grey900 } from 'material-ui/styles/colors';
 import TextField from 'material-ui/TextField';
 import FontIcon from 'material-ui/FontIcon';
 import React, { Component } from 'react';
 import request from 'superagent';
 import './App.css';
 
-
-const iconStyles = {
-  fontSize: 50,
+const iconStyles = { fontSize: 50
 };
 
 const inputStyles = {
-  errorStyle: {
-    color: 'red',
-  },
-  underlineStyle: {
-    borderColor: '#FFDEA8',
-  },
-  floatingLabelStyle: {
-    color: '#FFDEA8',
-  },
-  floatingLabelFocusStyle: {
-    color: '#FFDEA8',
-  },
-  inputStyle: {
-    color: '#FFDEA8',
-    fontSize: 20,
-  },
+    errorStyle: {
+        color: 'red'
+    },
+    underlineStyle: {
+        borderColor: '#FFDEA8'
+    },
+    floatingLabelStyle: {
+        color: '#FFDEA8'
+    },
+    floatingLabelFocusStyle: {
+        color: '#FFDEA8'
+    },
+    inputStyle: {
+        color: '#FFDEA8',
+        fontSize: 20
+    }
 };
 
 const btnStyles = {
-  button: {
-    backgroundColor: '#FFDEA8',
-  },
-  label: {
-    color: grey900,
-    label: 'Subscribe'
-  },
+    button: {
+        backgroundColor: '#FFDEA8'
+    },
+    label: {
+        color: grey900,
+        label: 'Subscribe'
+    }
 };
 
 class App extends Component {
-  constructor(props) {
-     super(props);
-      this.state = {
-        value: 0
-      };
+    constructor (props) {
+        super(props);
+        this.state = {
+            value: 0
+        };
     }
 
-  handleChange = (event) => {
-    this.setState({
-      value: event.target.value,
-    });
-  };
+    handleChange = (event) => {
+        this.setState({
+            value: event.target.value
+        });
+    };
 
-  handleSubscribe() {
-      (Number.isInteger(Number(this.state.value))) &&
-      (((this.state.value).toString().length) === 10) ?
-        request
+    handleSubscribe () {
+        (Number.isInteger(Number(this.state.value))) &&
+      (((this.state.value).toString().length) === 10)
+      ? request
            .post('https://1r12pvcy81.execute-api.us-east-1.amazonaws.com/production/subscribe')
            // .post('https://tzgvaie938.execute-api.us-east-1.amazonaws.com/dev/subscribe')
            // .post('subscribe')
            .query(`mobileNum=${Number(this.state.value)}`)
            .set('Accept', 'application/json')
            .set('Content-Type', 'application/json')
-           .end(function(err, res){
-             if (err || !res.ok) {
+           .end(function (err, res) {
+               if (err || !res.ok) {
                // alert(err)
-               alert('Oops. There seems to be a problem. Please confirm that you used your current mobile number')
-             } else
-               alert('Success! You should be receiving a confirmation text shortly.');
-           }): alert('Please enter a valid 10-digit mobile number.');
-  }
+                   alert('Oops. There seems to be a problem. Please confirm that you used your current mobile number')
+               } else { alert('Success! You should be receiving a confirmation text shortly.'); }
+           }) : alert('Please enter a valid 10-digit mobile number.');
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <div className={"text"}>
-          <FontIcon className="material-icons" style={iconStyles} color={'#FFDEA8'}>notifications</FontIcon>
-          <h1>Trail Alert</h1>
-          <h4>Sms trail-grooming updates </h4>
-          <TextField
-            id="input"
-            inputStyle={inputStyles.inputStyle}
-            floatingLabelText="Mobile number:"
-            floatingLabelStyle={inputStyles.floatingLabelStyle}
-            floatingLabelFocusStyle={inputStyles.floatingLabelFocusStyle}
-            underlineStyle={inputStyles.underlineStyle}
-            underlineFocusStyle={inputStyles.underlineStyle}
-            onChange={this.handleChange}
-
-
-          /><br/>
-          <RaisedButton
-            className="RsdBtn"
-            id="subscribe"
-            label='Subscribe'
-            labelPosition="before"
-            onClick={this.handleSubscribe.bind(this)}
-            primary={true}
-            icon={null}
-            buttonStyle={btnStyles.button}
-            labelStyle={btnStyles.label}
-
-          />
-        </div>
-      </div>
-    );
-  }
+    render () {
+        return (
+            <div className='App'>
+                <div className={'text'}>
+                    <FontIcon className='material-icons' style={iconStyles} color={'#FFDEA8'}>notifications</FontIcon>
+                    <h1>Trail Alert</h1>
+                    <h4>Sms trail-grooming updates </h4>
+                    <TextField
+                      id='input'
+                      inputStyle={inputStyles.inputStyle}
+                      floatingLabelText='Mobile number:'
+                      floatingLabelStyle={inputStyles.floatingLabelStyle}
+                      floatingLabelFocusStyle={inputStyles.floatingLabelFocusStyle}
+                      underlineStyle={inputStyles.underlineStyle}
+                      underlineFocusStyle={inputStyles.underlineStyle}
+                      onChange={this.handleChange}
+                    /><br />
+                    <RaisedButton
+                      className='RsdBtn'
+                      id='subscribe'
+                      label='Subscribe'
+                      labelPosition='before'
+                      onClick={this.handleSubscribe.bind(this)}
+                      primary={true}
+                      icon={null}
+                      buttonStyle={btnStyles.button}
+                      labelStyle={btnStyles.label}
+                    />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
-
 
--- serverless/handlers/twilio.js
+++ serverless/handlers/twilio.js
@@ -7,85 +7,77 @@ var twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
 AWS.config.update({ region: 'us-east-1' });
 
 exports.sendSmsMessages = sendSmsMessages;
-function sendSmsMessages( event, context, callback ) {
+function sendSmsMessages (event, context, callback) {
     var docClient = new AWS.DynamoDB.DocumentClient();
     async.waterfall([
-        function(callback) {
+        function (callback) {
             var params = {
-                    TableName: 'Grooming-Reports-dev',
-                    Key: {
-                      ID: 'Recently Groomed',
-                    }
+                TableName: 'Grooming-Reports-dev',
+                Key: {
+                    ID: 'Recently Groomed'
+                }
             };
             docClient.get(params, (err, status) => {
-               if (err) console.log(err, err.stack);
-               else callback(null, status);
+                if (err) console.log(err, err.stack);
+                else callback(null, status);
             });
         },
-        function(status, callback) {
-            var  paramsGetNums = {
+        function (status, callback) {
+            var paramsGetNums = {
                 TableName : 'User-Mobile-Numbers-dev',
                 AttributesToGet: [
-                   "mobileNumber",
+                    'mobileNumber'
                 ]
-              };
-            docClient.scan(paramsGetNums, function(err, nums) {
-               if (err) console.log(err, err.stack);
-               else callback && callback(null, nums, status.Item.Status);
+            };
+            docClient.scan(paramsGetNums, function (err, nums) {
+                if (err) console.log(err, err.stack);
+                else callback && callback(null, nums, status.Item.Status);
             });
         }
     ],
-        function(err, nums, status) {
-
-            var Message = status;
-            // console.log(Message)  //string
+        function (err, nums, status) {
             var mobileNums = nums.Items;
-            // console.log(mobileNums) //array of objects
             var Nums = mobileNums.map(number => number.mobileNumber);
-            // console.log(Nums)  //array of numbers
 
             for (var num of Nums) {
                 var sms = {
-                  to: num,
-                  body: status,
-                  from: twilioPhoneNumber
+                    to: num,
+                    body: status,
+                    from: twilioPhoneNumber
                 };
                 var twilioClient = require('twilio')(twilioAccountSid, twilioAuthToken);
-                // console.log(sms.to)
 
-                twilioClient.messages.create(sms, function(error, data) {
-                  // console.log(sms.to) //num
-                  if (error) {
-                    var errResponse = {
-                      headers: {
-                        'Access-Control-Allow-Origin': '*',
-                        "Access-Control-Allow-Credentials" : true
-                      },
-                      statusCode: error.status,
-                      body: {
-                        message: error.message,
-                        error: error
-                      },
+                twilioClient.messages.create(sms, function (error, data) {
+                    if (error) {
+                        var errResponse = {
+                            headers: {
+                                'Access-Control-Allow-Origin': '*',
+                                'Access-Control-Allow-Credentials' : true
+                            },
+                            statusCode: error.status,
+                            body: {
+                                message: error.message,
+                                error: error
+                            }
+                        };
+
+                        return callback && callback(null, JSON.stringify(errResponse));
+                    }
+                    console.log(`message: ${data.body}`);
+                    console.log(`date_created: ${data.date_created}`);
+
+                    var response = {
+                        statusCode: 200,
+                        headers: {
+                            'Access-Control-Allow-Origin': '*',
+                            'Access-Control-Allow-Credentials' : true
+                        },
+                        body: {
+                            message: 'Text message successfully sent!',
+                            data: data
+                        }
                     };
-
-                    return callback && callback(null, JSON.stringify(errResponse));
-                    console.log(`error: ${errResponse}`);
-                  }
-                  console.log(`message: ${data.body}`);
-                  console.log(`date_created: ${data.date_created}`);
-
-                  var response = {
-                    statusCode: 200,
-                    headers: {
-                      'Access-Control-Allow-Origin': '*',
-                      "Access-Control-Allow-Credentials" : true
-                    },
-                    body: {
-                      message: 'Text message successfully sent!',
-                      data: data
-                    },
-                  };
-                  return callback && callback(null, JSON.stringify(response));
+                    return callback && callback(null, JSON.stringify(response));
                 });
             }
         });
